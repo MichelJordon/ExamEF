b2a1477f67627ce038c769a6446eb4a36bae5c44
Michel <114498236+MichelJordon@users.noreply.github.com>
2022-12-11 20:26:37 +0500

    Exam

---
+++ Domain/Entities/JobGrades.cs
@@ -0,0 +1,6 @@
+namespace Domain.Entities;
+public class JobGrade{
+    public string GradeLevel { get; set; }
+    public int LowestSalary { get; set; }
+    public int HighestSalary { get; set; }
+}
---
+++ Domain/Entities/Region.cs
@@ -0,0 +1,8 @@
+namespace Domain.Entities;
+public class Region
+{
+    public int RegionId { get; set; }
+    public string RegionName { get; set; }
+
+    public virtual List<Country> Countries { get; set; }
+}
